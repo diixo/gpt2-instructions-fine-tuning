e32a0dd690620fb8e179b214a1ed5a0e8593ded9
diixo <anatolii.podoba@gmail.com>
2025-09-05 00:07:11 +0300

    explanation_1.py: initial commit

---
+++ explanation_1.py
@@ -0,0 +1,127 @@
+
+import json
+import os
+import torch
+
+
+def custom_collate_draft_2(batch, pad_token_id=50256, device="cpu"):
+    # Find the longest sequence in the batch
+    batch_max_length = max(len(item)+1 for item in batch)
+
+    # Pad and prepare inputs
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+
+        inputs = torch.tensor(padded[:-1])  # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:])  # Shift +1 to the right for targets
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    # Convert list of inputs to tensor and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+
+def custom_collate_fn(batch, pad_token_id=50256, ignore_index=-100, allowed_max_length=None, device="cpu"):
+    # Find the longest sequence in the batch
+    batch_max_length = max(len(item)+1 for item in batch)
+
+    # Pad and prepare inputs and targets
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+
+        inputs = torch.tensor(padded[:-1])  # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:])  # Shift +1 to the right for targets
+
+        # New: Replace all but the first padding tokens in targets by ignore_index
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        # New: Optionally truncate to maximum sequence length
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    # Convert list of inputs and targets to tensors and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+
+    return inputs_tensor, targets_tensor
+
+
+def test_logit_neg_100():
+
+    logits_1 = torch.tensor(
+        [[-1.0, 1.0],
+        [-0.5, 1.5]]
+    )
+    targets_1 = torch.tensor([0, 1])
+
+    loss_1 = torch.nn.functional.cross_entropy(logits_1, targets_1)
+    print(loss_1)
+
+    # -----------
+    logits_2 = torch.tensor(
+        [[-1.0, 1.0],
+        [-0.5, 1.5],
+        [-0.5, 1.5]]  # New 3rd training example
+    )
+    targets_2 = torch.tensor([0, 1, 1])
+
+    loss_2 = torch.nn.functional.cross_entropy(logits_2, targets_2)
+    print(loss_2)
+
+    # -----------
+    targets_3 = torch.tensor([0, 1, -100])  # ignore 3rd row
+
+    loss_3 = torch.nn.functional.cross_entropy(logits_2, targets_3)
+    print(loss_3)
+    print("loss_1 == loss_3:", loss_1 == loss_3)
+
+
+########################################################################
+from functools import partial
+
+customized_collate_fn = partial(
+    custom_collate_fn,
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+    allowed_max_length=1024
+)
+########################################################################
+
+if __name__ == "__main__":
+
+    inputs_1 = [0, 1, 2, 3, 4]
+    inputs_2 = [5, 6]
+    inputs_3 = [7, 8 , 9]
+
+    batch = [inputs_1, inputs_2, inputs_3]
+
+
+    inputs, targets = custom_collate_draft_2(batch)
+    print(inputs)
+    print(targets)
+
+    inputs, targets = custom_collate_fn(batch)
+    print(inputs)
+    print(targets)
+
+    test_logit_neg_100()

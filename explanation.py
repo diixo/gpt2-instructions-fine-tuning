a55ef0c33e8aaab51ac0ba50ee7351231b85547c
diixo <anatolii.podoba@gmail.com>
2025-09-05 06:58:08 +0300

    explanation.py: item_collate_fn implemented

--- explanation.py
+++ explanation.py
@@ -1,6 +1,5 @@
 
-import json
-import os
+import tiktoken
 import torch
 
 
@@ -66,6 +65,44 @@ def custom_collate_fn(batch, pad_token_id=50256, ignore_index=-100, allowed_max_
     return inputs_tensor, targets_tensor
 
 
+def item_collate_fn(batch, tokenizer, pad_token_id=50256, ignore_index=-100, allowed_max_length=None, device="cpu"):
+    input_ids_list, labels_list = [], []
+    max_len = 0
+
+    # Токенизация и маскирование
+    for item in batch:
+        question_ids = tokenizer.encode(item["question"])
+        answer_ids = tokenizer.encode(item["answer"]) + [pad_token_id]
+        # добавляю в конец принудительно endoftext, он не будет маскироваться на -100, потомучто является частью ответа
+
+        input_ids = question_ids + answer_ids
+        labels = [-100] * len(question_ids) + answer_ids  # маскируем вопрос
+
+        input_ids_list.append(input_ids)
+        labels_list.append(labels)
+        max_len = max(max_len, len(input_ids))
+
+    if allowed_max_length is not None and allowed_max_length >= max_len:
+        max_len = allowed_max_length
+    else:
+        # TODO:
+        max_len = max_len
+
+
+    # Padding
+    padded_inputs = []
+    padded_labels = []
+    for inp, lbl in zip(input_ids_list, labels_list):
+        padded_inputs.append(torch.tensor(inp + [pad_token_id] * (max_len - len(inp))))
+        padded_labels.append(torch.tensor(lbl + [ignore_index] * (max_len - len(lbl))))
+
+    inputs_tensor = torch.stack(padded_inputs).to(device)
+    labels_tensor = torch.stack(padded_labels).to(device)
+
+    return inputs_tensor, labels_tensor
+
+
+
 def test_logit_neg_100():
 
     logits_1 = torch.tensor(
@@ -101,19 +138,31 @@ def test_logit_neg_100():
 
 if __name__ == "__main__":
 
-    inputs_1 = [0, 1, 2, 3, 4]
-    inputs_2 = [5, 6]
-    inputs_3 = [7, 8 , 9]
+    if False:
+        inputs_1 = [0, 1, 2, 3, 4]
+        inputs_2 = [5, 6]
+        inputs_3 = [7, 8 , 9]
 
-    batch = [inputs_1, inputs_2, inputs_3]
+        batch = [inputs_1, inputs_2, inputs_3]
 
 
-    inputs, targets = custom_collate_draft_2(batch)
+        inputs, targets = custom_collate_draft_2(batch)
+        print(inputs)
+        print(targets)
+
+        print(24 * "*")
+        inputs, targets = custom_collate_fn(batch)
+        print(inputs)
+        print(targets)
+
+        #test_logit_neg_100()
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    batch = [
+        {"question": "Hello, Am I really language model", "answer": "yes"},
+        {"question": "Hello, Am I language model", "answer": "yes"},
+        ]
+    inputs, labels = item_collate_fn(batch, tokenizer, allowed_max_length=12)
     print(inputs)
-    print(targets)
-
-    inputs, targets = custom_collate_fn(batch)
-    print(inputs)
-    print(targets)
-
-    test_logit_neg_100()
+    print(labels)
